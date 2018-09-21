230c514581537ba070799ed326ecd85ef38f173b
pandadett <nuttadap@gmail.com>
2018-09-20 19:32:50 -0700

    Rename alignment.py as padthai.py

--- setup.py
+++ setup.py
@@ -128,7 +128,8 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    #packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    py_modules=["padthai"],
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
