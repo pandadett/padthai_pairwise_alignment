0d4e2f836249cf9f7bc2c9be3d487b3fd4895134
pandadett <nuttadap@gmail.com>
2018-09-20 19:36:07 -0700

    Update .travis_install.sh

--- .travis_install.sh
+++ .travis_install.sh
@@ -6,31 +6,6 @@ if [ $TRAVIS_OS_NAME == 'osx' ]; then
   #pip install numpy
 fi
 
-## setuptools < 18.0 has issues with Cython as a dependency
-#pip install Cython
-#if [ $? != 0 ]; then
-#    exit 1
-#fi
-
-# deps #FIXME: do better
-pip install pyyaml
-pip install numpy
-pip install scipy
-pip install pandas
-pip install xarray
-pip install scikit-learn
-pip install matplotlib
-pip install seaborn
-# NOTE: one day they shall fix this (sigh!)
-pip install Cython
-#pip install bhtsne
-# Google API tests are only local anyway
-#pip install google-api-python-client
-#pip install pkgconfig
-#pip install pybind11
-#pip install lshknn
-pip install numba
-pip install umap-learn
 
 # old setuptools also has a bug for extras, but it still compiles
 pip install -v '.'
